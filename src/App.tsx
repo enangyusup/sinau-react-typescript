4d7976a0c92ce81cebf215153fc149b50d413659
Enang Yusup <enang.yusup@gmail.com>
2022-02-10 17:48:28 +0700

    Create component title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Title from "./components/Title";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <Title title="Hello world" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
